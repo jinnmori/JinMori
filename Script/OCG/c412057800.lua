477684834fa9afd04b861d9fb0b2f7b31d4fe367
jinnmori <119871245+jinnmori@users.noreply.github.com>
2022-12-07 10:43:05 +0200

    Update c412057800.lua

--- Script/OCG/c412057800.lua
+++ Script/OCG/c412057800.lua
@@ -11,13 +11,14 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 s.listed_series={SET_DARK_MAGICIAN,SET_MAGICIAN_GIRL}
-function s.filter(c,e,tp)
+function s.filter(c)
 	return c:IsFaceup() and c:IsRace(RACE_SPELLCASTER)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+  if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil,e,tp) end
-	local tc=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil,e,tp):GetFirst()
+	and Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
+	local tc=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
 end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
