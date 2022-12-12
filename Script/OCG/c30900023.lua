43e2db4c50a551db2f3fd55b91797a4e34013e61
jinnmori <119871245+jinnmori@users.noreply.github.com>
2022-12-12 15:05:55 +0200

    Add files via upload

--- Script/OCG/c30900023.lua
+++ Script/OCG/c30900023.lua
@@ -1,6 +1,8 @@
 --Performapal Odd-eyes Chimera
 local s,id=GetID()
 function s.initial_effect(c)
+   --pendulum summon
+	Pendulum.AddProcedure(c,false)
 	--Fusion material
 	c:EnableReviveLimit()
 	Fusion.AddProcMixRep(c,true,true,s.mfilter,2,2)
@@ -34,6 +36,7 @@ function s.initial_effect(c)
 	e4:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
 	e4:SetDescription(aux.Stringid(id,1))
 	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCountLimit(1)
 	e4:SetProperty(EFFECT_FLAG_DELAY)
 	e4:SetCode(EVENT_BATTLE_DESTROYING)
 	e4:SetRange(LOCATION_MZONE)
@@ -43,7 +46,6 @@ function s.initial_effect(c)
 	c:RegisterEffect(e4)
 	--Place itself into pendulum zone
 	local e5=Effect.CreateEffect(c)
-	e5:SetDescription(aux.Stringid(id,2))
 	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e5:SetCode(EVENT_DESTROYED)
 	e5:SetProperty(EFFECT_FLAG_DELAY)
@@ -51,7 +53,29 @@ function s.initial_effect(c)
 	e5:SetTarget(s.pentg)
 	e5:SetOperation(s.penop)
 	c:RegisterEffect(e5)
-end
+	--Destroy 1 card in your Pendulum zone, place 1 Lvl 5 or lower "Performapal" or "Odd-eyes" from hand, Deck or GY
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_IGNITION)
+	e6:SetDescription(aux.Stringid(id,2))
+	e6:SetRange(LOCATION_PZONE)
+	e6:SetCategory(CATEGORY_DESTROY)
+	e6:SetCountLimit(1)
+	e6:SetCost(s.pncost)
+	e6:SetTarget(s.pntg)
+	e6:SetOperation(s.pnop)
+	c:RegisterEffect(e6)
+	--Discard 1 card, to prevent destruction for "Odd-Eyes" and "Performapal" monsters this turn.
+	local e7=Effect.CreateEffect(c)
+	e7:SetType(EFFECT_TYPE_QUICK_O)
+	e7:SetDescription(aux.Stringid(id,3))
+	e7:SetCode(EVENT_FREE_CHAIN)
+	e7:SetCountLimit(1)
+	e7:SetRange(LOCATION_PZONE)
+	e7:SetCost(s.incost)
+	e7:SetTarget(s.intg)
+	e7:SetOperation(s.inop)
+	c:RegisterEffect(e7)
+	end
 s.material_setcode={SET_PERFORMAPAL,SET_ODD_EYES}
 s.listed_series={SET_PERFORMAPAL,SET_ODD_EYES}
 
@@ -112,4 +136,53 @@ function s.penop(e,tp,eg,ep,ev,re,r,rp)
 	if c:IsRelateToEffect(e) then
 		Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
 	end
+end
+function s.pncost(e,tp,eg,ep,ev,re,r,rp,chk)
+   local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingMatchingCard(nil,tp,LOCATION_PZONE,0,1,c) end
+	local g=Duel.SelectMatchingCard(tp,nil,tp,LOCATION_PZONE,0,1,1,c)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,tp,LOCATION_PZONE)
+	Duel.Destroy(g,REASON_COST+REASON_EFFECT)
+	e:SetLabel(g:GetFirst():GetCode())
+end
+function s.pnfilter(c,code)
+   return c:IsSetCard({SET_ODD_EYES,SET_PERFORMAPAL}) and c:IsLevelBelow(5) and c:IsType(TYPE_PENDULUM) and not c:IsCode(code)
+end
+function s.pntg(e,tp,eg,ep,ev,re,r,rp,chk)
+   local all=LOCATION_DECK|LOCATION_HAND|LOCATION_GRAVE 
+	if chk==0 then return Duel.IsExistingMatchingCard(s.pnfilter,tp,all,0,1,nil,e:GetLabel()) end
+end
+function s.pnop(e,tp,eg,ep,ev,re,r,rp)
+   local all=LOCATION_DECK|LOCATION_HAND|LOCATION_GRAVE 
+   if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local g=Duel.SelectMatchingCard(tp,s.pnfilter,tp,all,0,1,1,nil,e:GetLabel())
+	if #g>0 then
+ Duel.MoveToField(g:GetFirst(),tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+    end
+ end
+ function s.incost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD,nil)
+end
+function s.infilter(c)
+ return c:IsSetCard({SET_PERFORMAPAL,SET_ODD_EYES}) and c:IsMonster()
+end
+function s.intg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.infilter,tp,LOCATION_MZONE,0,1,nil) end
+end
+function s.inop(e,tp,eg,ep,ev,re,r,rp)
+ 	local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetProperty(EFFECT_FLAG_IGNORE_RANGE)
+	e1:SetValue(1)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetTarget(aux.TargetBoolFunction(s.infilter))
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	Duel.RegisterEffect(e2,tp)
 end
