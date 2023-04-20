e2c55c49192e23a25ac21e586c885965b6113143
jinnmori <119871245+jinnmori@users.noreply.github.com>
2023-04-20 16:30:10 +0200

    Add files via upload

--- Script/OCG/c40905102.lua
+++ Script/OCG/c40905102.lua
@@ -28,7 +28,7 @@ function s.initial_effect(c)
 	--spsummon
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,2))
-	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetProperty(EFFECT_FLAG_DELAY)
 	e3:SetCode(EVENT_TO_GRAVE)
