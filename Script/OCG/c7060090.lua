041f7159a6d9349308ba6bfb1052dab5c4a8df40
jinnmori <119871245+jinnmori@users.noreply.github.com>
2023-04-20 15:55:47 +0200

    Update c7060090.lua

--- Script/OCG/c7060090.lua
+++ Script/OCG/c7060090.lua
@@ -1,60 +1,72 @@
---Inner Flame Heat
+--Infernity Re-Burning
 local s,id=GetID()
 function s.initial_effect(c)
-	--activate
+   --Activate 
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOGRAVE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+	--target 1 card your opponent controls send it to the GY
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCondition(s.gycon)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(s.gytg)
+	e2:SetOperation(s.gyop)
+	c:RegisterEffect(e2)
 end
-function s.filter(c,e,tp)
-	return c:IsFaceup() and c:IsSetCard(SET_INFERNITY) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
+s.listed_series={SET_INFERNITY}
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.PayLPCost(tp,math.floor(Duel.GetLP(tp)/2))
+	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
+	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
-		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)>0 end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,0,LOCATION_HAND)
 end
 function s.cfilter(c)
-	return c:IsSetCard(SET_INFERNITY) and c:IsLevelAbove(7) and c:IsLocation(LOCATION_MZONE)
+	return c:IsSetCard(SET_INFERNITY) and c:IsLocation(LOCATION_GRAVE)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if ft<=0 then return end
-	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_GRAVE,0,nil,e,tp)
-	while #g>0 and ft>0 do
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sg=g:Select(tp,ft,ft,nil)
-		local sg=Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
-		local o=Duel.GetOperatedGroup()
-		ft=ft-1
-	local ct=o:Filter(s.cfilter,nil)
-	for tc in aux.Next(ct) do
-	--Effect of Xyz Level 
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_XYZ_LEVEL)
-	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetValue(s.xyzlv)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-	tc:RegisterEffect(e1)
-				end
-		end
-end
-function s.xyzlv(e,c,rc)
-	if rc:IsType(TYPE_XYZ) then
-		return 12,e:GetHandler():GetLevel()
-	else
-		return e:GetHandler():GetLevel()
-			end
+	local g=Duel.GetFieldGroup(tp,LOCATION_HAND,0)
+	Duel.SendtoGrave(g,REASON_EFFECT)
+	local o=Duel.GetOperatedGroup()
+	local ct=o:FilterCount(s.cfilter,nil)
+	if ct>0 then
+		Duel.BreakEffect()
+	--instead of Drawing 1 card; Draw 1 card for each infernity sent this way
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_DRAW_COUNT)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetValue(ct+1)
+	e1:SetReset(RESET_PHASE+PHASE_DRAW+RESET_SELF_TURN,1)
+	Duel.RegisterEffect(e1,tp)
 	end
+end
+function s.gycon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetFieldGroupCount(tp,LOCATION_HAND,0)==0 and Duel.GetTurnCount()~=e:GetHandler():GetTurnID() or e:GetHandler():IsReason(REASON_RETURN)
+end
+function s.gytg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_ONFIELD) end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectTarget(tp,s.filter,tp,0,LOCATION_ONFIELD,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function s.gyop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+	Duel.SendtoGrave(tc,REASON_EFFECT)
+	end
+end
