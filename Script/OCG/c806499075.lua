c78d9c5f8fadb3649aa2f96019c7efe78dad4173
jinnmori <119871245+jinnmori@users.noreply.github.com>
2023-05-07 17:34:10 +0300

    Fixed Card "NEO"
    The additional Effect that prevents it from shuffling back to extra has been fixed

--- Script/OCG/c806499075.lua
+++ Script/OCG/c806499075.lua
@@ -1,5 +1,4 @@
 --NEO
---Scripted by Eerie Code
 local s,id=GetID()
 function s.initial_effect(c)
 	--activate
@@ -11,41 +10,31 @@ function s.initial_effect(c)
 	e1:SetCost(s.cost)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-	--return
-	local e5=Effect.CreateEffect(c)
-	e5:SetCategory(CATEGORY_TODECK)
-	e5:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
-	e5:SetRange(LOCATION_REMOVED)
-	e5:SetCode(EVENT_PHASE+PHASE_END)
-	e5:SetCode(EFFECT_SEND_REPLACE)
-	e5:SetCountLimit(1,id+1)
-	e5:SetTarget(s.reptg)
-	c:RegisterEffect(e5)
 --Neos EnableNeosReturn 
-	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TODECK)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e1:SetCode(EVENT_PHASE+PHASE_END)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1,id+2)
-	e1:SetCondition(Auxiliary.NeosReturnCondition1)
-	e1:SetTarget(Auxiliary.NeosReturnTarget(c,extrainfo))
-	e1:SetOperation(Auxiliary.NeosReturnOperation(c,extraop))
-	c:RegisterEffect(e1)
-	local e2=e1:Clone()
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(0)
-	e2:SetCondition(Auxiliary.NeosReturnCondition2)
-	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_TODECK)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_PHASE+PHASE_END)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetCondition(Auxiliary.NeosReturnCondition1)
+	e3:SetTarget(Auxiliary.NeosReturnTarget(c,extrainfo))
+	e3:SetOperation(Auxiliary.NeosReturnOperation(c,extraop))
+	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(0)
+	e4:SetCondition(Auxiliary.NeosReturnCondition2)
+	c:RegisterEffect(e4)
 	if returneff then
-		e1:SetLabelObject(returneff)
-		e2:SetLabelObject(returneff)
+		e3:SetLabelObject(returneff)
+		e4:SetLabelObject(returneff)
 	end
 end
 s.listed_names={CARD_NEOS}
-s.listed_series={0x1f}
+s.listed_series={SET_NEO_SPACIAN}
 function s.filter2(c,e,tp)
-	return c:IsSetCard(0x1f) and c:IsAbleToGrave() and c:IsCanBeFusionMaterial()
+	return c:IsSetCard(SET_NEO_SPACIAN) and c:IsAbleToGrave() and c:IsCanBeFusionMaterial()
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:SetLabel(1)
@@ -60,7 +49,7 @@ function s.filter(c,e,tp,mc)
 if Duel.GetLocationCountFromEx(tp,tp,mc,c)<=0 then return false end
 	local mustg=aux.GetMustBeMaterialGroup(tp,nil,tp,c,nil,REASON_FUSION)
 	return c:IsType(TYPE_FUSION) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SPECIAL,tp,true,false)
-		and aux.IsMaterialListCode(c,mc:GetCode()) and (#mustg==0 or (#mustg==1 and mustg:IsContains(mc)))
+		and Card.ListsCodeAsMaterial(c,mc:GetCode()) and (#mustg==0 or (#mustg==1 and mustg:IsContains(mc)))
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
@@ -90,16 +79,22 @@ function Auxiliary.NeosReturnTarget(c,extrainfo)
 		if extrainfo then extrainfo(e,tp,eg,ep,ev,re,r,rp,chk) end
 	end
 end
-function Auxiliary.NeosReturnSubstituteFilter(c)
-	return c:IsCode(14088859) and c:IsAbleToRemoveAsCost() or c:IsCode(806499075) and c:IsAbleToRemoveAsCost()
+function Auxiliary.NeosReturnSubstituteFilter1(c)
+	return c:IsCode(14088859) and c:IsAbleToRemoveAsCost()
+end
+function Auxiliary.NeosReturnSubstituteFilter2(c)
+	return c:IsCode(id) and c:IsAbleToDeckAsCost()
 end
 function Auxiliary.NeosReturnOperation(c,extraop)
 	return function(e,tp,eg,ep,ev,re,r,rp)
 		local c=e:GetHandler()
 		if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
-		local sc=Duel.GetFirstMatchingCard(Auxiliary.NecroValleyFilter(Auxiliary.NeosReturnSubstituteFilter),tp,LOCATION_GRAVE,0,nil)
-		if sc and Duel.SelectYesNo(tp,aux.Stringid(806499075,0)) then
-			Duel.Remove(sc,POS_FACEUP,REASON_COST)
+		local sc1=Duel.GetFirstMatchingCard(Auxiliary.NecroValleyFilter(Auxiliary.NeosReturnSubstituteFilter1),tp,LOCATION_GRAVE,0,nil)
+		local sc2=Duel.GetFirstMatchingCard(Auxiliary.NecroValleyFilter(Auxiliary.NeosReturnSubstituteFilter2),tp,LOCATION_GRAVE,0,nil)
+		if sc1 and Duel.SelectYesNo(tp,aux.Stringid(14088859,0)) then
+			Duel.Remove(sc1,POS_FACEUP,REASON_COST)
+		elseif c:ListsCodeAsMaterial(CARD_NEOS) and sc2 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+			Duel.SendtoDeck(sc2,nil,2,REASON_COST)
 		else
 			Duel.SendtoDeck(c,nil,2,REASON_EFFECT)
 		end
@@ -109,9 +104,4 @@ function Auxiliary.NeosReturnOperation(c,extraop)
 			end
 		end
 	end
-end
-function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return c:GetDestination()==(LOCATION_REMOVED) end
-		Duel.SendtoDeck(e:GetHandler(),nil,2,REASON_EFFECT)
 end
