8000145391f33c0641eb318d9c29615e8186b1f7
jinnmori <119871245+jinnmori@users.noreply.github.com>
2023-08-19 09:42:29 +0300

    Add files via upload

---
+++ Script/OCG/c6689910.lua
@@ -0,0 +1,108 @@
+--Supreme King Arc Dragon Odd-Eyes
+local s,id=GetID()
+function s.initial_effect(c)
+	--Pendulum summon
+	Pendulum.AddProcedure(c)
+	--Special Summon itself from the hand if you have a card in your Pendulum zone
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	c:RegisterEffect(e1)
+		--atkup
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,SET_ODD_EYES))
+	e2:SetValue(1000)
+	c:RegisterEffect(e2)
+	--Also they are unaffected by your opponent's card effects
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_IMMUNE_EFFECT)
+	e3:SetValue(s.efilter)
+	c:RegisterEffect(e3)
+		--Inflict piercing damage
+	local e4=e2:Clone()
+	e4:SetDescription(3208)
+	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+	e4:SetCode(EFFECT_PIERCE)
+	e4:SetValue(DOUBLE_DAMAGE)
+	c:RegisterEffect(e4)
+	--Add 1 "Odd-Eyes" monster 
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,0))
+	e5:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e5:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e5:SetCode(EVENT_DESTROYED)
+	e5:SetCondition(s.thcon)
+	e5:SetTarget(s.thtg)
+	e5:SetOperation(s.thop)
+	c:RegisterEffect(e5)
+	--Add 1 "Odd-Eyes" Pendulum Monster 
+	local p1=Effect.CreateEffect(c)
+	p1:SetDescription(aux.Stringid(id,0))
+	p1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	p1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	p1:SetRange(LOCATION_PZONE)
+	p1:SetCode(EVENT_DESTROYED)
+	p1:SetCountLimit(1,id)
+	p1:SetCondition(s.thcon2)
+	p1:SetTarget(s.thtg2)
+	p1:SetOperation(s.thop2)
+	c:RegisterEffect(p1)
+end
+s.listed_series={SET_ODD_EYES}
+s.listed_names={16178681}
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(nil,tp,LOCATION_PZONE,0,1,nil)
+end
+function s.efilter(e,re)
+	return e:GetOwnerPlayer()~=re:GetOwnerPlayer()
+end
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return rp~=tp and e:GetHandler():IsPreviousControler(tp)
+end
+function s.thfilter(c)
+	return c:IsSetCard(SET_ODD_EYES) and c:IsMonster() and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function s.cfilter(c,tp)
+	return c:IsReason(REASON_EFFECT) and c:GetPreviousControler()==tp
+end
+function s.thcon2(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,e:GetHandler(),tp)
+end
+function s.thfilter2(c)
+	return c:IsSetCard(SET_ODD_EYES) and c:IsType(TYPE_PENDULUM) and c:IsMonster() and c:IsAbleToHand()
+end
+function s.thtg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter2,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop2(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter2,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
