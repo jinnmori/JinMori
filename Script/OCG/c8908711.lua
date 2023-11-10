3618ed6e35b59a680217fa4159e4b53a61332633
jinnmori <119871245+jinnmori@users.noreply.github.com>
2023-11-10 13:07:11 +0200

    Fixed new card

--- Script/OCG/c8908711.lua
+++ Script/OCG/c8908711.lua
@@ -4,14 +4,39 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_AQUA),2,2)
 	--disable
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(0,LOCATION_MZONE)
+	e1:SetTarget(s.distg)
+	e1:SetCode(EFFECT_DISABLE)
+	c:RegisterEffect(e1)
+	--tograve
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_TOGRAVE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e2:SetTarget(s.tgtg)
+	e2:SetOperation(s.tgop)
+	e2:SetCountLimit(1,id)
+	c:RegisterEffect(e2)
+	--Send the top 3 cards from your top deck
 	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetTargetRange(0,LOCATION_MZONE)
-	e3:SetTarget(s.distg)
-	e3:SetCode(EFFECT_DISABLE)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_DECKDES)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetCondition(s.grcon)
+	e3:SetTarget(s.grtg)
+	e3:SetOperation(s.grop)
 	c:RegisterEffect(e3)
 end
+s.listed_series={SET_TEARLAMENTS}
+s.listed_names={id}
 function s.pointfilter(c,tp,lg)
 	return c:IsType(TYPE_FUSION) and lg:IsContains(c)
 end
@@ -20,5 +45,29 @@ function s.distg(e,c)
 	local lg=e:GetHandler():GetLinkedGroup()
 	local g=Duel.GetMatchingGroup(s.pointfilter,tp,LOCATION_MZONE,0,nil,tp,lg)
 	local atk=g:GetSum(Card.GetAttack)
-	return #g>0 and c:IsType(TYPE_MONSTER) or (c:GetOriginalType()&TYPE_EFFECT)==TYPE_EFFECT and g:GetSum(Card.GetLevel)==c:GetAttack()
+	return #g>0 and c:GetAttack()<=atk and (c:IsType(TYPE_MONSTER) or (c:GetOriginalType()&TYPE_EFFECT)==TYPE_EFFECT) 
+end
+function s.tgfilter(c)
+	return c:IsSetCard(SET_TEARLAMENTS) and c:IsAbleToGrave()
+end
+function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOGRAVE,nil,1,tp,LOCATION_DECK)
+end
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoGrave(g,REASON_EFFECT)
+	end
+end
+function s.grcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsReason(REASON_EFFECT)
+end
+function s.grtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,3) end
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,3)
+end
+function s.grop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.DiscardDeck(tp,3,REASON_EFFECT)
 end
